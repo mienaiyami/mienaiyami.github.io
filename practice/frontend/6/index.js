82451fd4517706dee64d84743d634fe600bdbad3
SukoonT <84740082+SukoonT@users.noreply.github.com>
2021-06-02 03:29:31 +0530

    6

---
+++ practice/frontend/6/index.js
@@ -0,0 +1,51 @@
+const slider = document.querySelector("#price-slider");
+const checkMorY = document.querySelector("#toggleym");
+$(".pricepm .price-span").html(`$${slider.value}.0`);
+
+$(".p-total").css(
+    "width",
+    slider.clientWidth - parseFloat($(".price-slider").css("--slider-width"))
+);
+
+$(".p-current").css({
+    width:
+        (slider.value / slider.max) *
+        (parseFloat(slider.clientWidth) -
+            parseFloat($(".price-slider").css("--slider-width"))),
+    left:
+        slider.offsetLeft +
+        parseFloat($(".price-slider").css("--slider-width")) / 2,
+});
+slider.oninput = () => {
+    $(".pricepm .price-span").html(`$${slider.value}`);
+    console.log(slider.value);
+    $(".p-current").css({
+        width:
+            (slider.value / slider.max) *
+            (parseFloat(slider.clientWidth) -
+                parseFloat($(".price-slider").css("--slider-width"))),
+        left:
+            slider.offsetLeft +
+            parseFloat($(".price-slider").css("--slider-width")) / 2,
+    });
+};
+checkMorY.onchange = () => {
+    if (checkMorY.checked) {
+        slider.value *= 12;
+        console.log(slider.value);
+        slider.step *= 12;
+        slider.max *= 12;
+        $(".pricepm").html(
+            `<span class="price-span">$0</span><span>/year</span>`
+        );
+    }
+    if (!checkMorY.checked) {
+        slider.value /= 12;
+        slider.step /= 12;
+        slider.max /= 12;
+        $(".pricepm").html(
+            `<span class="price-span">$0</span><span>/month</span>`
+        );
+    }
+};
+console.log(slider.value);
