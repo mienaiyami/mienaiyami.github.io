77c674ce4262fca00f28d7965b8e4e226d28dc98
SukoonT <84740082+SukoonT@users.noreply.github.com>
2021-06-23 03:05:46 +0530

    17 ray caster

--- practice/canvas/app.js
+++ practice/canvas/app.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 let textToIndex = `<!DOCTYPE html>
 <html lang="en">
     <head>
@@ -8,7 +9,7 @@ let textToIndex = `<!DOCTYPE html>
     </head>
     <body>
         <div>`;
-const canvasFiles = fs.readdirSync("./");
+const canvasFiles = fs.readdirSync(path.join(__dirname, "/"));
 for (let i = 0; i < canvasFiles.length; i++) {
     textToIndex += `
     <a href="https://bzfohi9xdxb4ahltdqxx0g-on.drv.tw/TechBlazed/practice/js/canvas/${canvasFiles[i]}">canvas/${canvasFiles[i]}</a><br><br>
@@ -20,5 +21,6 @@ textToIndex += `
 </body>
 </html>
 `;
-fs.writeFileSync("./index.html", textToIndex);
+fs.writeFileSync(path.join(__dirname, "index.html"), textToIndex);
 console.log("lol");
+console.log(canvasFiles);
