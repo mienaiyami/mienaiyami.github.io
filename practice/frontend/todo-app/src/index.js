d3c4cb62b031390c351e3b0d3d9ceae634579fe1
SukoonT <sukoonkumar2@gmail.com>
2021-06-26 11:50:11 +0530

    todo app

---
+++ practice/frontend/todo-app/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import App from "./components/App.jsx";
+
+console.log("contact me at sukoonkumar2@gmail.com");
+console.log("https://github.com/SukoonT");
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
